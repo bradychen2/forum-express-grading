756d0e0f9159de599fef9fd4883c8a023bbd84f8
bradychen <bradychen2@gmail.com>
2021-04-02 11:10:40 +0800

    feat: add admin create form & admin restaurant post

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,6 +8,24 @@ const adminController = {
       .then(restaurants => {
         return res.render('admin/restaurants', { restaurants })
       })
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+    if (!name) {
+      req.flash('error_msgs', 'Name is required')
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name, tel, address, opening_hours, description
+    })
+      .then(restaurant => {
+        req.flash('success_msgs', 'Restaurant was successfully created')
+        return res.redirect('/admin/restaurants')
+      })
   }
 }
 
