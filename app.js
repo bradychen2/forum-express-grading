e47880aaf2b78f9b61f03877c9ad6b1aac269b91
bradychen <bradychen2@gmail.com>
2021-04-02 22:29:40 +0800

    fix: precess.env.PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({
   extname: 'hbs',
