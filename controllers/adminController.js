9ac4a1d865cdd77a7df8e384d25e03c7e5aef830
bradychen <bradychen2@gmail.com>
2021-04-10 16:47:29 +0800

    feat: show category name on restaurants.hbs and detail page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,14 +1,18 @@
 const db = require('../models')
-const restaurant = require('../models/restaurant')
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true })
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
       .then(restaurants => {
         return res.render('admin/restaurants', { restaurants })
       })
@@ -49,9 +53,13 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true })
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
       .then(restaurant => {
-        return res.render('admin/restaurant', { restaurant })
+        return res.render('admin/restaurant', {
+          restaurant: restaurant.toJSON()
+        })
       })
   },
 
