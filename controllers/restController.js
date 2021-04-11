756bd7437061b12f53644e9840cdc95bc462472f
bradychen <bradychen2@gmail.com>
2021-04-11 12:27:39 +0800

    feat: add front desk show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -18,6 +18,17 @@ const restController = {
       })
       res.render('restaurants', { restaurants: data })
     })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
   }
 }
 
