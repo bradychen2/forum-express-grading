cdd454f504d7d9dee0aea03340cd7ed2af3f5555
bradychen <bradychen2@gmail.com>
2021-04-16 21:25:56 +0800

    feat: add pagination on restaurants index & fix seeder img

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,24 +1,41 @@
 db = require('../models')
 Restaurant = db.Restaurant
 Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       // Transform to Number for sequelize query
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
 
-    return Restaurant.findAll({
+    return Restaurant.findAndCountAll({
       raw: true,
       nest: true,
       include: [Category],
-      where: whereQuery
-    }).then(restaurants => {
-      const data = restaurants.map(r => {
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(results => {
+      const page = req.query.page || 1 // url no params, default page 1
+      // Cal how much pages
+      const pages = Math.ceil(results.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 < pages ? pages : page + 1
+
+      // Cut description to length-50 and add categoryName for render
+      const data = results.rows.map(r => {
         return {
           ...r,
           description: r.description.substring(0, 50),
@@ -32,7 +49,8 @@ const restController = {
         res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          page, totalPage, prev, next
         })
       })
     })
