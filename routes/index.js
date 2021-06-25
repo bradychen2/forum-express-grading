55ae20372cb620fce9632fbc0574cabeac4fb147
bradychen <bradychen2@gmail.com>
2021-06-25 15:11:37 +0800

    modify: error handling

--- routes/index.js
+++ routes/index.js
@@ -68,4 +68,27 @@ module.exports = (app, passport) => {
     userController.signIn)
   app.get('/logout', userController.logout)
 
+  // --------------------Error--------------------
+  // 404 Not Found
+  app.use((req, res, next) => {
+    res.status(404)
+
+    if (req.accepts('json')) {
+      res.json({ error: '404 - Not Found' })
+      return
+    }
+    res.type('text/plain').send('404 - Not Found')
+  })
+
+  // 500 Server Error
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500)
+
+    if (req.accepts('json')) {
+      res.json({ error: '500 - Server Error' })
+      return
+    }
+    res.type('text/plain').send('500 - Server Error')
+  })
 }
